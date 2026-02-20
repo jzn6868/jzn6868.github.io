082c19802face9d4da3c83ca20ac32f5a551ec4d
jzn6868 <zenan.jiang@cityu.edu.hk>
2026-02-20 15:41:52 +0000

    Deploying to gh-pages from @ jzn6868/jzn6868.github.io@eb146988148c2c30836102702ace2d911e8b6289 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -380,10 +380,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-just-landed-in-hong-kong-today",
-          title: 'I just landed in Hong Kong today.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
@@ -391,7 +388,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-i-just-landed-in-hong-kong-today",
+          title: 'I just landed in Hong Kong today.',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_1/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
