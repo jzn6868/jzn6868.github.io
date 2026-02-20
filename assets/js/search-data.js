700507ec928639c4859466505787b9df38ce1e88
jzn6868 <zenan.jiang@cityu.edu.hk>
2026-02-20 16:16:11 +0000

    Deploying to gh-pages from @ jzn6868/jzn6868.github.io@9593000db783ba9dbabdeb23db599efd079dd486 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -373,15 +373,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-just-landed-in-hong-kong-today",
+            },},{id: "news-i-just-landed-in-hong-kong-today",
           title: 'I just landed in Hong Kong today.',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1/";
-            },},{id: "projects-project-1",
+            },},{id: "news-",
+          title: '',
+          description: "",
+          section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
