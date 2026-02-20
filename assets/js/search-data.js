f82f6c97f0a47fb757cdb8e478058114c7077172
jzn6868 <zenan.jiang@cityu.edu.hk>
2026-02-20 16:25:35 +0000

    Deploying to gh-pages from @ jzn6868/jzn6868.github.io@7d14310a10437fde5545e9408911c3609e89b9d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -378,10 +378,12 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1/";
-            },},{id: "news-",
-          title: '',
+            },},{id: "news-my-first-time-having-heytea-in-mong-kok",
+          title: 'My first time having Heytea in Mong Kok.',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
